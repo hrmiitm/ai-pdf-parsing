358c46fe0ef19b007cf846218740c09840491899
hritik@hrm <121506757+hrmiitm@users.noreply.github.com>
2026-01-16 23:37:22 +0530

    bounding box
    Removed unnecessary blank lines in the main.py file.

--- main.py
+++ main.py
@@ -7,22 +7,16 @@ import fitz
 class BoundingBoxField(BaseModel):
     bounding_box: list[int] = Field(..., description='The bounding box where the information was found [y_min, x_min, y_max, x_max]')
     page: int = Field(..., description='Page number where the information was found. Start counting with 1.')
-
 class TotalAmountField(BoundingBoxField):
     value: float = Field(..., description='The total amount of the invoice.')
-
 class RecipientField(BoundingBoxField):
     name: str = Field(..., description='The name of the recipient.')
-
 class TaxAmountField(BoundingBoxField):
     value: float = Field(..., description='The total amount of the tax.')
-
 class SenderField(BoundingBoxField):
     name: str = Field(..., description='The name of the sender.')
-
 class AccountNumberField(BoundingBoxField):
     account_no: str = Field(..., description='The number of the account.')
-
 
 class InvoiceModel(BaseModel):
     total: TotalAmountField
@@ -90,7 +84,6 @@ def main():
     Extract the invoice recipient name and invoice total.
     Return only json that matches the provided schema.
     """
-
     print(f"2. AI Analysis on PDF started")
     invoice = ai_analysis(pdf_file_loc, prompt, ai_model_name)
     # print(invoice)
